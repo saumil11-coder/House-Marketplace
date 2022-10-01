32e67897c9787608440a811947c10ee0f90418d7
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-10-01 16:01:21 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center bg-dark text-light p-4">
+      <h6>Saumil Patel </h6>
+    </div>
+  );
+};
+
+export default Footer;
